802a2095af72f04da76bc1293e95ec0acd5e8216
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2024-06-26 12:07:44 +0530

    Assignment Done question 1

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -25,7 +25,7 @@ def test_adjust_recipe():
     assert adjust_recipe(2, 8) == 4.0
     assert adjust_recipe(0, 5) == 0.0
     assert adjust_recipe(1, 1) == 2.0
-    # Add more test cases as neede
+    # Add more test cases as needed
 
 
 # Test cases for total_cost function
